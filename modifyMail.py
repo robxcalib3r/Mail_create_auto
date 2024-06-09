b890fd9011ef3b24cc3e19ce7531732b63805150
Robxcalib3r <khisarobin@gmail.com>
2024-06-09 16:16:28 +0600

    feat: add full contact info

--- modifyMail.py
+++ modifyMail.py
@@ -13,11 +13,6 @@ import re
 
 class modify():
     def __init__(self, driver, _waitTime):
-        # self.corpLoc = "Plot-1088,R-80ft.2, Block-I, Khilkhet, Vatara, Bashundhara R/A"
-        # self.hqLoc = "Chandra, kaliakoir, Gazipur"
-        # self.whiplc = "Walton Hi-Tech Industries PLC."
-        # self.wdil = "Walton Digi-Tech Industries Ltd."
-
         self.ou = {
             'WHIPLC': "Walton Hi-Tech Industries PLC.",
             'WDTIL': "Walton Digi-Tech Industries Ltd.",
@@ -27,6 +22,20 @@ class modify():
             'WPZT': "Walton Plaza",
             'RBG': "RB Group of Companies Ltd.",
             'WSL': "Walton Shipping & Logistics"
+        }
+
+        self.hq = {
+            'street': 'Chandra, kaliakoir, Gazipur',
+            'city':'Dhaka',
+            'state': 'Gazipur',
+            'zip': '1710',
+        }
+
+        self.corp = {
+            'street': 'Plot-1088,R-80ft.2, Block-I, Khilkhet, Vatara, Bashundhara R/A',
+            'city':'Dhaka',
+            'state': 'Dhaka',
+            'zip': '1229',
         }
 
         self.driver = driver
@@ -71,26 +80,70 @@ class modify():
 
 
     def modifyContact(self, _phone, _location):
-        # Updates contact information
+        ## Updates contact information
 
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[@name='ContactInformation']"))).click()
         time.sleep(self.waitTime)
 
-        # Updates Phone Number
+        ## Updates Phone Number
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxPhone']"))).clear()
         WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxPhone']"))).send_keys(_phone)
         time.sleep(self.waitTime)
 
-        # # Updates Office location
-        # WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxPhone']"))).send_keys(Keys.PAGE_DOWN)
-        # WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_moreOptionsContact_label']"))).click()
-        # # WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[@uid='MoreOptionsContactLabel']"))).send_keys(Keys.PAGE_DOWN)
-        # WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).clear()
-        # if bool(re.search('corporate', _location.lower())):
-        #     WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).send_keys(self.corpLoc)
-        # elif bool(re.search('headquarters', _location.lower())):
-        #     WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).send_keys(self.hqLoc)
-        # time.sleep(self.waitTime)
+        if bool(re.search('headquarters', _location.lower())):
+            contact_update = True
+            details = self.hq
+        elif bool(re.search('(corporate|base)', _location.lower())):
+            contact_update = True
+            details = self.corp
+        else:
+            contact_update = False
+            details = None
+
+        if contact_update:    
+            ## Updates Street
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//textarea[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxStreetAddress']"))).clear()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//textarea[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxStreetAddress']"))).send_keys(details['street'])
+
+            ## Updates City
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxCity']"))).clear()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxCity']"))).send_keys(details['city'])
+
+            ## Updates State/Province
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxStateOrProvince']"))).clear()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxStateOrProvince']"))).send_keys(details['state'])
+
+            ## Updates zip
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxPostalCode']"))).clear()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_tbxPostalCode']"))).send_keys(details['zip'])
+
+            ## Updates Country region
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//select[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_ctl00_dplCountryOrRegion']"))).click()
+            time.sleep(self.waitTime)
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//option[@value='BD']"))).click()
+            
+
+            ## Updates Office location
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxPhone']"))).send_keys(Keys.PAGE_DOWN)
+            time.sleep(self.waitTime)
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_moreOptionsContact_label']"))).click()
+            time.sleep(self.waitTime)
+            # WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[@uid='MoreOptionsContactLabel']"))).send_keys(Keys.PAGE_DOWN)
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).clear()
+            
+            ## v1.0
+            # if bool(re.search('corporate', _location.lower())):
+            #     WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).send_keys(self.corpLoc)
+            # elif bool(re.search('headquarters', _location.lower())):
+            #     WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).send_keys(self.hqLoc)
+            ## v2.0
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxOffice']"))).send_keys(details['street'])
+            time.sleep(self.waitTime)
+
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxWebPage']"))).clear()
+            WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='ResultPanePlaceHolder_Mailbox_ContactInformation_contentContainer_ContactInformationPlaceHolder_tbxWebPage']"))).send_keys("https://www.waltonbd.com")
+            time.sleep(self.waitTime)
+            # input('waiting.....')
 
 
     def saveInfo(self):
