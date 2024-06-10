dc6d0c597e026a71914ab8b6565442861557e4a3
Robxcalib3r <khisarobin@gmail.com>
2024-06-10 16:42:40 +0600

    feat: range to limit processed emails

--- active_deactive_mail_check.py
+++ active_deactive_mail_check.py
@@ -35,12 +35,15 @@ def check_inactive(filename, save_inactives):
     active = open(filename, 'r')
 
     s = search(driverEx, 0.2)
+    count = 1
     for line in active:
         _, enabled = s.disabledMailCheck(line)
         if not enabled:
             # print(f'{line} not found!')
             with open(save_inactives, 'a') as file:
                 file.writelines(f'{line}')
+            print(f'remaining mails: {len(active) - count}')
+            count += 1
 
 def check_active(filename, save_actives):
     ## Initializing webdriver for HRMS
@@ -62,6 +65,31 @@ def check_active(filename, save_actives):
             # print(f'{line} not found!')
             with open(save_actives, 'a') as file:
                 file.writelines(f'{line}')
+
+def check_active_special(filename, save_actives, initLine, endLine):
+    ## Initializing webdriver for HRMS
+    init = init_driver()
+    ## Initializing webdriver for Exchange
+    driverEx = init.setup_method()
+
+    # Logging in to Exchange
+    _uE, _pE = credentials('credentials.txt', 5, 6)
+    signInEx_ = login(driverEx)
+    signInEx_.loginExchange(_uE, _pE)
+
+    activeFile = open(filename, 'r')
+    activeFile_lines = activeFile.readlines()
+
+    s = search(driverEx, 0.1)
+    for lineNo in range(initLine, endLine):
+        input(activeFile_lines[lineNo])
+        newline = activeFile_lines[lineNo].split('\t')[1]
+        _, enabled = s.disabledMailCheck(newline)
+        print(newline)
+        if enabled:
+            # print(f'{line} not found!')
+            with open(save_actives, 'a') as file:
+                file.writelines(f'{activeFile_lines[lineNo]}')
 
 if __name__ == '__main__':
     check_active('inactive_employees_from_shell.txt', 'active_employees_from_shell.txt')
