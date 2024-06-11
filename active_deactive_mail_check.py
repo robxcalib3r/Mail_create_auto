d3d0d8426e37d91e0b33f2cd8fad9f2941b2119c
Robxcalib3r <khisarobin@gmail.com>
2024-06-11 12:10:11 +0600

    to check active, inactive and corrupted employee and feat: Individualized checking, Process percentage, lineNo.

--- active_deactive_mail_check.py
+++ active_deactive_mail_check.py
@@ -4,6 +4,8 @@ import time
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import os
 
 ## Supporting modules
 from login import login
@@ -90,6 +92,46 @@ def check_active_special(filename, save_actives, initLine, endLine):
             # print(f'{line} not found!')
             with open(save_actives, 'a') as file:
                 file.writelines(f'{activeFile_lines[lineNo]}')
+
+def checkActiveInactiveCorrupted(filename, activeSaveFile, inactiveSaveFile, corruptSaveFile, initLine, endLine):
+    ## Initializing webdriver for HRMS
+    init = init_driver()
+    ## Initializing webdriver for Exchange
+    driverEx = init.setup_method()
+
+    # Logging in to Exchange
+    _uE, _pE = credentials('credentials.txt', 5, 6)
+    signInEx_ = login(driverEx)
+    signInEx_.loginExchange(_uE, _pE)
+
+    mailList = open(filename, 'r')
+    mailListLines = mailList.readlines()
+
+    s = search(driverEx, 0.1)
+    count = 1
+    for lineNo in range(initLine, endLine):
+        # input(mailListLines[lineNo])
+        email = mailListLines[lineNo].split('\t')[1]
+        while True:
+            try:
+                _, status = s.disabledMailCheck(email)
+                break
+            except TimeoutException:
+                print(f'!!!! -----TimeOutException------!!!!')
+                driverEx.refresh()
+        percent = int(count/(endLine - initLine)*100)
+        print(f'{percent}%--{lineNo} : {email} :       {status}')
+        if status == 'Enabled':
+            # print(f'{line} not found!')
+            with open(os.getcwd() + activeSaveFile, 'a') as file:
+                file.writelines(f'{mailListLines[lineNo]}')
+        elif status == 'Corrupted':
+            with open(os.getcwd() + corruptSaveFile, 'a') as cfile:
+                cfile.writelines(f'{mailListLines[lineNo]}')
+        elif status == 'Disabled':
+            with open(os.getcwd() + inactiveSaveFile, 'a') as inacFile:
+                inacFile.writelines(f'{mailListLines[lineNo]}')
+        count +=1
 
 if __name__ == '__main__':
     check_active('inactive_employees_from_shell.txt', 'active_employees_from_shell.txt')
