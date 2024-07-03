2cf90db973d1721c53feea56931d8ea253bb357b
Robxcalib3r <khisarobin@gmail.com>
2024-07-03 17:52:21 +0600

    fix: miniscule issue

--- core/searchMailbox.py
+++ core/searchMailbox.py
@@ -9,7 +9,6 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import TimeoutException
-import time
 import re
 
 class search():
